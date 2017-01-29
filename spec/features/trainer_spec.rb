b857a74dec48a30599f708bf23d07bb61648d9cd
AAAleg <awwert@yandex.ru>
2017-01-29 17:59:28 +0900

    Revert "Refactor trainer_spec.rb"
    This reverts commit e81b67bedff619bd7e879848b2b21192bd7c3eb8.

--- spec/features/trainer_spec.rb
+++ spec/features/trainer_spec.rb
@@ -4,6 +4,9 @@ require 'support/helpers/trainer_helper.rb'
 include LoginHelper
 include TrainerHelper
 
+update_review_date = ->(card){card.update_attribute(:review_date,
+                                                     Time.now - 3.days) }
+
 describe 'review cards without blocks' do
   it_behaves_like 'training without cards' do
     let(:user) { [:user] }
@@ -17,7 +20,10 @@ describe 'review cards with one block' do
 
   describe 'training with two cards' do
     before do
-      prepare(:user_with_one_block_and_two_cards)
+      user = create(:user_with_one_block_and_two_cards)
+      user.cards.each(&update_review_date)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with two cards'
@@ -25,7 +31,10 @@ describe 'review cards with one block' do
 
   describe 'training with one card' do
     before do
-      prepare(:user_with_one_block_and_one_card)
+      user = create(:user_with_one_block_and_one_card)
+      user.cards.each(&update_review_date)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with one card'
@@ -61,7 +70,10 @@ describe 'review cards with two blocks' do
 
   describe 'training with two cards' do
     before do
-      prepare(:user_with_two_blocks_and_one_card_in_each)
+      user = create(:user_with_two_blocks_and_one_card_in_each)
+      user.cards.each(&update_review_date)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with two cards'
@@ -69,7 +81,10 @@ describe 'review cards with two blocks' do
 
   describe 'training with one card' do
     before do
-      prepare(:user_with_two_blocks_and_only_one_card)
+      user = create(:user_with_two_blocks_and_only_one_card)
+      user.cards.each(&update_review_date)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with one card'
@@ -83,7 +98,13 @@ describe 'review cards with current_block' do
   
   describe 'training with two cards' do
     before do
-      prepare_for_current_block(:user_with_two_blocks_and_two_cards_in_each)
+      user = create(:user_with_two_blocks_and_two_cards_in_each)
+      block = user.blocks.first
+      user.set_current_block(block)
+      card = user.cards.find_by(block_id: block.id)
+      card.update_attribute(:review_date, Time.now - 3.days)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with two cards'
@@ -91,7 +112,13 @@ describe 'review cards with current_block' do
 
   describe 'training with one card' do
     before do
-      prepare_for_current_block(:user_with_two_blocks_and_one_card_in_each)
+      user = create(:user_with_two_blocks_and_one_card_in_each)
+      block = user.blocks.first
+      user.set_current_block(block)
+      card = user.cards.find_by(block_id: block.id)
+      card.update_attribute(:review_date, Time.now - 3.days)
+      visit trainer_path
+      login('test@test.com', '12345', 'Войти')
     end
 
     it_behaves_like 'training with one card'
