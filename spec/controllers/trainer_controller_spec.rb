7b42ff9d64f19ce91cc30ded09673a5e5bf40d3b
AAAleg <awwert@yandex.ru>
2017-02-05 21:33:52 +0900

    back tests

--- spec/controllers/trainer_controller_spec.rb
+++ spec/controllers/trainer_controller_spec.rb
@@ -11,121 +11,121 @@ describe Dashboard::TrainerController do
       @controller.send(:auto_login, @user)
     end
 
-    it 'calls CheckTranslation' do
-      # allow(CheckTranslation).to receive(:call).with(card: @card, user_translation: 'house')
-      allow_any_instance_of(CheckTranslation).to receive(:call).with(card: @card, user_translation: 'house')
+    # it 'calls CheckTranslation' do
+    #   # allow(CheckTranslation).to receive(:call).with(card: @card, user_translation: 'house')
+    #   allow_any_instance_of(CheckTranslation).to receive(:call).with(card: @card, user_translation: 'house')
 
-      put :review_card, params: { card_id: @card.id, user_translation: 'house' }
+    #   put :review_card, params: { card_id: @card.id, user_translation: 'house' }
 
-      expect(CheckTranslation).to have_received(:call)
+    #   expect(CheckTranslation).to have_received(:call)
+    # end
+
+    context 'correct translation' do
+      it_behaves_like 'correct translation' do
+        let(:card_factory) { [:card, user: @user, block: @block] }
+        let(:expected_repeat) { 2 }
+        let(:interval) { 1 }
+        let(:expected_interval) { 6 }
+        let(:efactors) { [2.6, 2.18, 1.5] }
+      end
+
+      it_behaves_like 'correct translation' do
+        let(:card_factory) { [:card, :second_check, user: @user,
+                              block: @block] }
+        let(:expected_repeat) { 3 }
+        let(:interval) { 6 }
+        let(:expected_interval) { 16 }
+        let(:efactors) { [2.7, 2.28, 1.6] }
+      end
+
+      it 'repeat=1-3 quality=5' do
+        card = create(:card, user: @user, block: @block,
+                      interval: 1, repeat: 1, efactor: 2.5, quality: 5)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'house', 1)
+        expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
+            to eq((Time.zone.now + 16.days).strftime('%Y-%m-%d %H:%M'))
+        expect(card.interval).to eq(45)
+        expect(card.repeat).to eq(4)
+        expect(card.attempt).to eq(1)
+        expect(card.efactor).to eq(2.8)
+        expect(card.quality).to eq(5)
+      end
     end
 
-    # context 'correct translation' do
-    #   it_behaves_like 'correct translation' do
-    #     let(:card_factory) { [:card, user: @user, block: @block] }
-    #     let(:expected_repeat) { 2 }
-    #     let(:interval) { 1 }
-    #     let(:expected_interval) { 6 }
-    #     let(:efactors) { [2.6, 2.18, 1.5] }
-    #   end
+    context 'incorrect translation' do
+      it_behaves_like 'incorrect translation' do
+        let(:attempt) { 1 }
+        let(:efactor) { 2.18 }
+        let(:quality) { 2 }
+      end
 
-    #   it_behaves_like 'correct translation' do
-    #     let(:card_factory) { [:card, :second_check, user: @user,
-    #                           block: @block] }
-    #     let(:expected_repeat) { 3 }
-    #     let(:interval) { 6 }
-    #     let(:expected_interval) { 16 }
-    #     let(:efactors) { [2.7, 2.28, 1.6] }
-    #   end
+      it_behaves_like 'incorrect translation' do
+        let(:attempt) { 2 }
+        let(:efactor) { 1.64 }
+        let(:quality) { 1 }
+      end
 
-    #   it 'repeat=1-3 quality=5' do
-    #     card = create(:card, user: @user, block: @block,
-    #                   interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'house', 1)
-    #     expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-    #         to eq((Time.zone.now + 16.days).strftime('%Y-%m-%d %H:%M'))
-    #     expect(card.interval).to eq(45)
-    #     expect(card.repeat).to eq(4)
-    #     expect(card.attempt).to eq(1)
-    #     expect(card.efactor).to eq(2.8)
-    #     expect(card.quality).to eq(5)
-    #   end
-    # end
+      it_behaves_like 'incorrect translation' do
+        let(:attempt) { 3 }
+        let(:efactor) { 1.3 }
+        let(:quality) { 0 }
+      end
+    end
 
-    # context 'incorrect translation' do
-    #   it_behaves_like 'incorrect translation' do
-    #     let(:attempt) { 1 }
-    #     let(:efactor) { 2.18 }
-    #     let(:quality) { 2 }
-    #   end
+    context 'correct and incorrect translation' do
+      it 'repeat=1-3 quality=4' do
+        card = create(:card, user: @user, block: @block,
+                      interval: 1, repeat: 1, efactor: 2.5, quality: 4)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'RoR', 1)
+        card = check_review_card(card, 'house', 1)
+        expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
+            to eq((Time.zone.now + 1.days).strftime('%Y-%m-%d %H:%M'))
+        expect(card.interval).to eq(6)
+        expect(card.repeat).to eq(2)
+        expect(card.attempt).to eq(1)
+        expect(card.efactor).to eq(2.38)
+        expect(card.quality).to eq(4)
+      end
 
-    #   it_behaves_like 'incorrect translation' do
-    #     let(:attempt) { 2 }
-    #     let(:efactor) { 1.64 }
-    #     let(:quality) { 1 }
-    #   end
+      it 'repeat=1-3 quality=5' do
+        card = create(:card, user: @user, block: @block,
+                      interval: 1, repeat: 1, efactor: 2.5, quality: 4)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'RoR', 1)
+        card = check_review_card(card, 'house', 1)
+        card.update(review_date: Time.zone.now)
+        card = check_review_card(card, 'house', 1)
+        expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
+            to eq((Time.zone.now + 6.days).strftime('%Y-%m-%d %H:%M'))
+        expect(card.interval).to eq(14)
+        expect(card.repeat).to eq(3)
+        expect(card.attempt).to eq(1)
+        expect(card.efactor).to eq(2.38)
+        expect(card.quality).to eq(5)
+      end
 
-    #   it_behaves_like 'incorrect translation' do
-    #     let(:attempt) { 3 }
-    #     let(:efactor) { 1.3 }
-    #     let(:quality) { 0 }
-    #   end
-    # end
-
-    # context 'correct and incorrect translation' do
-    #   it 'repeat=1-3 quality=4' do
-    #     card = create(:card, user: @user, block: @block,
-    #                   interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'RoR', 1)
-    #     card = check_review_card(card, 'house', 1)
-    #     expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-    #         to eq((Time.zone.now + 1.days).strftime('%Y-%m-%d %H:%M'))
-    #     expect(card.interval).to eq(6)
-    #     expect(card.repeat).to eq(2)
-    #     expect(card.attempt).to eq(1)
-    #     expect(card.efactor).to eq(2.38)
-    #     expect(card.quality).to eq(4)
-    #   end
-
-    #   it 'repeat=1-3 quality=5' do
-    #     card = create(:card, user: @user, block: @block,
-    #                   interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'RoR', 1)
-    #     card = check_review_card(card, 'house', 1)
-    #     card.update(review_date: Time.zone.now)
-    #     card = check_review_card(card, 'house', 1)
-    #     expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-    #         to eq((Time.zone.now + 6.days).strftime('%Y-%m-%d %H:%M'))
-    #     expect(card.interval).to eq(14)
-    #     expect(card.repeat).to eq(3)
-    #     expect(card.attempt).to eq(1)
-    #     expect(card.efactor).to eq(2.38)
-    #     expect(card.quality).to eq(5)
-    #   end
-
-    #   it 'repeat=3 attempt=4' do
-    #     card = create(:card, user: @user, block: @block,
-    #                   interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-    #     card = check_review_card(card, 'RoR', 3)
-    #     card = check_review_card(card, 'house', 1)
-    #     expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-    #         to eq((Time.zone.now + 1.days).strftime('%Y-%m-%d %H:%M'))
-    #     expect(card.interval).to eq(6)
-    #     expect(card.repeat).to eq(2)
-    #     expect(card.attempt).to eq(1)
-    #     expect(card.efactor).to eq(1.3)
-    #     expect(card.quality).to eq(3)
-    #   end
-    # end
+      it 'repeat=3 attempt=4' do
+        card = create(:card, user: @user, block: @block,
+                      interval: 16, repeat: 3, efactor: 2.7, quality: 5)
+        card = check_review_card(card, 'RoR', 3)
+        card = check_review_card(card, 'house', 1)
+        expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
+            to eq((Time.zone.now + 1.days).strftime('%Y-%m-%d %H:%M'))
+        expect(card.interval).to eq(6)
+        expect(card.repeat).to eq(2)
+        expect(card.attempt).to eq(1)
+        expect(card.efactor).to eq(1.3)
+        expect(card.quality).to eq(3)
+      end
+    end
   end
 end
